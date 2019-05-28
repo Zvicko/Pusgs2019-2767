cc08c16dee7756086ecd29153996406a2ff41432
Zvicko <33682321+Zvicko@users.noreply.github.com>
2019-05-28 13:39:24 +0200

    added some repos

---
+++ WebApp/WebApp/Persistence/Repository/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Persistence.Repository
+{
+    public interface IUserRepository : IRepository<User,int>
+    {
+        IEnumerable<User> GetAll(int pageIndex, int pageSize);
+
+    }
+}
---
+++ WebApp/WebApp/Persistence/Repository/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApp.Models;
+
+namespace WebApp.Persistence.Repository
+{
+    public class UserRepository : Repository<User, int>, IUserRepository
+    {
+        public UserRepository(DbContext context) : base(context)
+        {
+
+        }
+
+        protected ApplicationDbContext DemoContext { get { return context as ApplicationDbContext; } }
+        public IEnumerable<User> GetAll(int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
