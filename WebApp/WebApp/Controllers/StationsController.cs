5b8080ea00dc332971faed4e5fc0d4b2ae2d7094
SBArtist <33682385+SBArtist@users.noreply.github.com>
2019-05-31 16:49:08 +0200

    WebApp update - Controllers
    Added some controllers.

---
+++ WebApp/WebApp/Controllers/StationsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApp.Models;
+using WebApp.Persistence;
+using WebApp.Persistence.UnitOfWork;
+
+namespace WebApp.Controllers
+{
+    public class StationsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private readonly IUnitOfWork unitOfWork;
+
+        public StationsController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Stations
+        public IEnumerable<Station> GetStations()
+        {
+            return unitOfWork.Stations.GetAll() ;
+        }
+
+        // GET: api/Stations/5
+        [ResponseType(typeof(Station))]
+        public IHttpActionResult GetStation(int id)
+        {
+            Station station = db.Stations.Find(id);
+            if (station == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(station);
+        }
+
+        // PUT: api/Stations/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutStation(int id, Station station)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != station.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(station).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Stations
+        [ResponseType(typeof(Station))]
+        public IHttpActionResult PostStation(Station station)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Stations.Add(station);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = station.Id }, station);
+        }
+
+        // DELETE: api/Stations/5
+        [ResponseType(typeof(Station))]
+        public IHttpActionResult DeleteStation(int id)
+        {
+            Station station = db.Stations.Find(id);
+            if (station == null)
+            {
+                return NotFound();
+            }
+
+            db.Stations.Remove(station);
+            db.SaveChanges();
+
+            return Ok(station);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StationExists(int id)
+        {
+            return db.Stations.Count(e => e.Id == id) > 0;
+        }
+    }
+}
