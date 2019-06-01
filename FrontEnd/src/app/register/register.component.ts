7c8eff65dec62ccd2e594e015b5a19916f4cdd28
Zvicko <33682321+Zvicko@users.noreply.github.com>
2019-06-01 11:12:09 +0200

    added custom validation for Birthdate

--- FrontEnd/src/app/register/register.component.ts
+++ FrontEnd/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormControl,Validators,FormBuilder} from '@angular/forms';
+import {FormGroup,FormControl,Validators,FormBuilder,AbstractControl} from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -12,8 +12,35 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
   this.userForm = this.fb.group({
     fullName : ['',[Validators.required,Validators.minLength(6),Validators.maxLength(40)]],
-
+    email: ['',[Validators.required,Validators.email]],
+    birthdate : ['',[Validators.required,validateDate]]
   })
+  }
+
+
+// function validateDate(control: AbstratctControl) : {[key: string]:any} | null{
+//   const date : string  = control.value;
+
+
+//   return null;
+
+// }
+
+}
+function validateDate(control: AbstractControl) : {[key: string]:any} | null{
+  const dateString : string  = control.value;
+  let minYear = new Date().getFullYear() - 12;
+  let maxYear = new Date().getFullYear() - 100;
+  let date = new Date(dateString);
+  // alert(date);
+  if((date.getFullYear() > minYear || date.getFullYear() < maxYear) && date != null)
+  {
+
+    return {'validateDate':true};
+  }
+  else
+  {
+    return null;
   }
 
 }
